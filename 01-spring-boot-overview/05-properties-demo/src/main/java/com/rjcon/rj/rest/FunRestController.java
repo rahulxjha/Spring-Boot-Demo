8fb2766ef9e5112f67672de66d25a2d7fd61e173
Rahul Jha <rjrahuljha02@gmail.com>
2023-05-12 21:09:38 +0530

    Spring Properties

--- 01-spring-boot-overview/05-properties-demo/src/main/java/com/rjcon/rj/rest/FunRestController.java
+++ 01-spring-boot-overview/05-properties-demo/src/main/java/com/rjcon/rj/rest/FunRestController.java
@@ -1,10 +1,26 @@
 package com.rjcon.rj.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for " coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "team info"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach Name = " + this.coachName + ", Team Name = " + this.teamName;
+    }
+
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello(){
@@ -23,14 +39,5 @@ public class FunRestController {
         return "Today is your lucky day";
     }
 
-    @GetMapping("/motivation")
-    public String getDailyMotivation(){
-        return "Always remember why have you started!";
-    }
-
-    @GetMapping("/task")
-    public String getDailyTask(){
-        return "You have to buy vegetables.";
-    }
     
 }
